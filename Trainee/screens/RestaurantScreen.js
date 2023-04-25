61790c2c3e300c444af17a008bc244bfcdafe4f3
Pakeg Bae <pakegpakeg@gmail.com>
2023-04-25 21:39:34 +0000

    Update 11 files

---
+++ Trainee/screens/RestaurantScreen.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { ScrollView, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  ArrowLeftIcon,
+  StarIcon,
+  MapPinIcon,
+  QuestionMarkCircleIcon,
+  ChevronRightIcon,
+} from "react-native-heroicons/solid";
+
+import DishRow from "../components/DishRow";
+
+export default function RestaurantScreen({ route: { params }, navigation }) {
+  return (
+    <ScrollView>
+      <View className="relative">
+        <Image
+          alt={params.title}
+          source={params.imgSrc}
+          className="w-full h-56 bg-gray-300 p-4"
+        />
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
+        >
+          <ArrowLeftIcon size={20} color="#00CCBB" />
+        </TouchableOpacity>
+      </View>
+
+      <View className="bg-white">
+        <View className="px-4 pt-4">
+          <Text className="text-3xl font-bold">{params.title}</Text>
+          <View className="flex-row space-x-2 my-1">
+            <View className="flex-row items-center space-x-1">
+              <StarIcon color="green" opacity={0.5} size={22} />
+              <Text className="text-xs text-gray-500">
+                <Text className="text-green-500">{params.rating}</Text>{" "}
+                {params.category.map(
+                  (item, idx) =>
+                    item.name + (params.category.length - 1 == idx ? "" : " / ")
+                )}
+              </Text>
+            </View>
+
+            <View className="flex-row items-center space-x-1">
+              <MapPinIcon color="gray" opacity={0.4} size={22} />
+              <Text className="text-xs text-gray-500">
+                Nearby {params.address}
+              </Text>
+            </View>
+          </View>
+          <Text className="text-gray-500 mt-2 pb-4">{params.short_desc}</Text>
+        </View>
+        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+          <Text className="flex-1 pl-2 text-md font-bold">
+            Have a food allergy?
+          </Text>
+          <ChevronRightIcon color="#00CCBB" />
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+
+        <DishRow></DishRow>
+      </View>
+    </ScrollView>
+  );
+}
+// id,
+//           imgSrc,
+//           title,
+//           rating,
+//           genre,
+//           address,
+//           short_desc,
+//           dishes,
+//           long,
+//           lat,
---
+++ Trainee/store/featureStore.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    name: "Offers near you!!!",
+    desc: "Discover exclusive discounts on your favorite products and services in your local area. Don't miss out on great deals and savings that are just around the corner!",
+    restaurant: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    id: 2,
+    name: "Discounts up to 15 pm",
+    desc: "Shop now and take advantage of our limited-time offer with discounts up to 15% off on selected items. Don't wait, grab these amazing deals while they last!",
+    restaurant: [2, 4, 6],
+  },
+  {
+    id: 3,
+    name: "Most popular dish",
+    desc: "Our restaurant's most popular dish is the savory and delicious chicken parmigiana. It's the perfect blend of tender chicken, tangy tomato sauce, and melted cheese that will leave your taste buds wanting more",
+    restaurant: [1, 3, 5],
+  },
+  {
+    id: 4,
+    name: "Best today's",
+    desc: "Our chef's special of the day is the mouth-watering grilled salmon with a side of roasted vegetables. It's the perfect healthy and delicious meal to fuel your day. Don't miss out on this fantastic dish!",
+    restaurant: [1, 3, 4, 6],
+  },
+];
